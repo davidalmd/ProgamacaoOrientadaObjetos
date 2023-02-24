3bd9e60e652dda7db8638469cf7485d14ebdd328
cLausx0 <david.almeida2707@gmail.com>
2023-02-23 21:30:21 -0300

    Envio da Atividade da Aula 03

---
+++ Aula03 - Classes e Objetos/Ex02/Movie.java
@@ -0,0 +1,9 @@
+public class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt() {
+        System.out.println("O filme em exibição no momento é: " + title );
+    }
+}
