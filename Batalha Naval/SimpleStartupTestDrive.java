0e13650410669693017b25a31bd9a91dc3be2718
David Lucas Alves de Almeida <20221ewbj0173@belojardim.ifpe>
2023-03-10 10:51:36 -0300

    Adicionando atividade de batalha naval

---
+++ Batalha Naval/SimpleStartupTestDrive.java
@@ -0,0 +1,18 @@
+public class SimpleStartupTestDrive {
+    public static void main(String[] args) {
+        SimpleStartup dot = new SimpleStartup();
+        int [] locations = {2, 3, 4};
+        dot.setLocationCells(locations);
+        int userGuess = 2;
+        // int userGuess1 = 2;
+        // int userGuess2 = 2;
+        String result = dot.checkYourself(userGuess);
+        // String result1 = dot.checkYourself(userGuess1);
+        // String result2 = dot.checkYourself(userGuess2);
+        String testResult = "failed";
+    if (result.equals("hit")) {/*|| result1.equals("hit") || result2.equals("hit"))*/} {
+            testResult = "passed";
+        }
+        System.out.println(testResult);
+    }
+}
