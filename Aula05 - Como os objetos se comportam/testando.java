a55fd386bf46a67a5dfa2e51add2e2b6cc886e93
cLausx0 <david.almeida2707@gmail.com>
2023-03-13 01:45:17 -0300

    Adicionando pastas da Aula 05

---
+++ Aula05 - Como os objetos se comportam/testando.java
@@ -0,0 +1,25 @@
+public class testando {
+    int calcArea(int height, int width) {
+        return height * width;
+    }
+public static void main(String[] args) {
+
+        int a = calcArea(7, 8);
+        short c = 7;
+        calcArea(c, 15);    
+
+        int d = calcArea(57);
+
+        calcArea(2, 3);
+
+        long t = 42;
+        int f = calcArea(t, 17);
+
+        int g = calcArea();
+
+        calcArea();
+
+        byte h = calcArea(4, 20);
+        int j = calcArea(2, 3, 5);
+    }
+}
--- Batalha Naval/GameHelper.java
+++ Batalha Naval/GameHelper.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class GameHelper {
     public int getUserInput(String prompt){
-        System.out.println(prompt + ": ");
+        System.out.println(prompt);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
