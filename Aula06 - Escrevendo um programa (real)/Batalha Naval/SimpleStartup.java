d103445db69fb588a86cf656f58cfb5085e3ef05
David Lucas Alves de Almeida <20221ewbj0173@belojardim.ifpe>
2023-03-17 10:29:48 -0300

    Bug da Batalha Naval Corrigido

--- Aula06 - Escrevendo um programa (real)/Batalha Naval/SimpleStartup.java
+++ Aula06 - Escrevendo um programa (real)/Batalha Naval/SimpleStartup.java
@@ -1,34 +1,59 @@
-class SimpleStartup {
-    private int[] locationCells;
-    private int numOfHits = 0;
-    private int[] hited = new int[3];
-    private int contador = 0;
+import java.util.ArrayList;
 
-    public void setLocationCells(int[] locs) {
+class SimpleStartup {
+    private ArrayList<String> locationCells;
+
+    //#region Código antigo
+    // private int[] locationCells;
+    // private int numOfHits = 0;
+    // private int[] hited = new int[3];
+    // private int contador = 0;
+    //#endregion
+
+    public void setLocationCells(ArrayList<String> locs) {
         locationCells = locs;
     }
 
-    public String checkYourself(int guess) {
+    //#region Código antigo
+    // public String checkYourself(int guess) {
+    //     String result = "miss";
+    //     for (int cell : locationCells){
+    //         if (guess == cell){
+    //             if ((guess == hited[0]) || (guess == hited[1]) || (guess == hited[2])) {
+    //                 System.out.println("You already hit this cell!");
+    //                 result = "miss";
+    //                 break;
+    //             }
+    //             else{
+    //                 hited[contador] = guess;
+    //                 contador++;
+    //                 result = "hit";
+    //                 numOfHits++;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     if (numOfHits == locationCells.length){
+    //         result = "kill";
+    //     }
+    //     System.out.println(result);
+    //     return result;
+    // }
+    //#endregion
+    
+    public String checkYourself(String userInput) {
         String result = "miss";
-        for (int cell : locationCells){
-            if (guess == cell){
-                if ((guess == hited[0]) || (guess == hited[1]) || (guess == hited[2])) {
-                    System.out.println("You already hit this cell!");
-                    result = "miss";
-                    break;
-                }
-                else{
-                    hited[contador] = guess;
-                    contador++;
-                    result = "hit";
-                    numOfHits++;
-                    break;
-                }
+        int index = locationCells.indexOf(userInput);
+        
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+            } else {
+                result = "hit";
             }
         }
-        if (numOfHits == locationCells.length){
-            result = "kill";
-        }
+
         System.out.println(result);
         return result;
     }
--- Aula06 - Escrevendo um programa (real)/Batalha Naval/SimpleStartupGame.java
+++ Aula06 - Escrevendo um programa (real)/Batalha Naval/SimpleStartupGame.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class SimpleStartupGame {
     public static void main(String[] args) {
         int numOfGuesses = 0;
@@ -6,13 +8,17 @@ public class SimpleStartupGame {
         SimpleStartup theStartup =  new SimpleStartup();
         int randomNum = (int) (Math.random() * 5);
 
-        int [] location = {randomNum, randomNum + 1, randomNum + 2};
+        // int [] location = {randomNum, randomNum + 1, randomNum + 2};
+        ArrayList<String> location = new ArrayList<String>();
+        location.add(Integer.toString(randomNum));
+        location.add(Integer.toString(randomNum + 1));
+        location.add(Integer.toString(randomNum + 2));
         theStartup.setLocationCells(location);
 
         boolean isAlive = true;
 
         while (isAlive) {
-            int guess = helper.getUserInput("enter a number: ");
+            String guess = helper.getUserInput("enter a number: ");
             String result = theStartup.checkYourself(guess);
             numOfGuesses++;
             if (result.equals("kill")){
